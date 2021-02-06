dde0177598914280bc655cb1a00a048d84081906
Matsuda Takuya <matsuda.takuya41@gmail.com>
2021-02-07 00:41:45 +0900

    [fix] displayHand関数をモジュール化

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { Card } from "./types/Card";
 import { calcSum } from "./modules/calcSum";
 import { clearResult } from "./modules/clearResult";
 import { createDeck } from "./modules/createDeck";
+import { displayHand } from "./modules/displayHand";
 import { displayMoney } from "./modules/displayMoney";
 import { firstDeal } from "./modules/firstDeal";
 import { setBet } from "./modules/setBet";
@@ -45,48 +46,6 @@ const displayPlayersNewCard = async () => {
     );
     console.log("");
   }
-};
-
-const displayHand = async (
-  dealersHand: Card[],
-  playersHand: Card[]
-): Promise<void> => {
-  let dealer = colors.bold(`Dealer: `);
-  let player = colors.bold(`You:    `);
-  for (let i = 0; i < dealersHand.length; i++) {
-    if (dealersHand[i].isOpen) {
-      if (dealersHand[i].symbol === "♥" || dealersHand[i].symbol === "♦") {
-        dealer += colors.red.bgWhite(
-          ` ${dealersHand[i].symbol} ${dealersHand[i].rank} `
-        );
-      } else {
-        dealer += colors.black.bgWhite(
-          ` ${dealersHand[i].symbol} ${dealersHand[i].rank} `
-        );
-      }
-    } else {
-      dealer += colors.black.bgWhite(" ??? ");
-    }
-    dealer += "  ";
-  }
-  for (let i = 0; i < playersHand.length; i++) {
-    if (playersHand[i].symbol === "♥" || playersHand[i].symbol === "♦") {
-      player += colors.red.bgWhite(
-        ` ${playersHand[i].symbol} ${playersHand[i].rank} `
-      );
-    } else {
-      player += colors.black.bgWhite(
-        ` ${playersHand[i].symbol} ${playersHand[i].rank} `
-      );
-    }
-    player += "  ";
-  }
-  console.log(dealer);
-  console.log("");
-  console.log(player);
-  console.log("");
-  readlineSync.question(colors.bold("(Enter)"));
-  console.log("");
 };
 
 const displayDealersSecondCard = async () => {
@@ -287,7 +246,7 @@ const initGame = async () => {
     dealersHand,
     playersHand
   ));
-  // await displayHand(dealersHand, playersHand);
+  await displayHand(dealersHand, playersHand);
   // await checkPlayersHand();
   // initGame();
 };
