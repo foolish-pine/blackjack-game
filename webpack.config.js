2a2f6fc48320518001a58d441f61e53902db4483
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-02-11 21:38:52 +0900

    [fix] tsconfig、webpack.configを必要最小限になるよう修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,16 @@
 module.exports = {
-  watch: true,
   mode: "development",
-  target: 'node',
+  target: "node",
   entry: "./src/main.ts",
   module: {
     rules: [
       {
-        enforce: 'pre',
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader",
-        options: {
-          fix: true
-        }
-      },
-      {
         test: /\.ts$/,
         loader: "ts-loader",
-        exclude: /node_modules/,
-        options: {
-          configFile: "webpack.tsconfig.json"
-        }
-      }
-    ]
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.js']
-  }
+    extensions: [".ts", ".js"],
+  },
 };
