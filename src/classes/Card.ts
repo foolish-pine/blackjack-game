e03766c6d1e2485713d9cd00af10ef979695acff
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-02-11 21:46:13 +0900

    [update] Cardクラスの初期化を省略して記述

--- src/classes/Card.ts
+++ src/classes/Card.ts
@@ -1,13 +1,9 @@
 export class Card {
-  private _symbol: string;
-  private _number: number;
-  private _rank: string;
-
-  constructor(symbol: string, number: number, rank: string) {
-    this._symbol = symbol;
-    this._number = number;
-    this._rank = rank;
-  }
+  constructor(
+    private readonly _symbol: string,
+    private readonly _number: number,
+    private readonly _rank: string
+  ) {}
 
   get symbol(): string {
     return this._symbol;
