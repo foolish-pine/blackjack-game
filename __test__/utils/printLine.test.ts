7af505071f03db6e45cf389d3c07e3c5460db200
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-02-12 22:12:16 +0900

    [fix] printLine関数のテスト中のモック化処理修正

--- __test__/utils/printLine.test.ts
+++ __test__/utils/printLine.test.ts
@@ -2,9 +2,11 @@ import { printLine } from "../../src/utils/printLine";
 
 describe("printLine関数", () => {
   it("printLine()メソッドに渡された引数を引数にして、process.stdout.write()メソッドを1度呼び出す", () => {
-    const spy = jest.spyOn(process.stdout, "write");
+    const mockProcessStdoutWrite = jest
+      .spyOn(process.stdout, "write")
+      .mockImplementation();
     printLine("text");
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenCalledWith("text");
+    expect(mockProcessStdoutWrite).toHaveBeenCalledTimes(1);
+    expect(mockProcessStdoutWrite).toHaveBeenCalledWith("text");
   });
 });
