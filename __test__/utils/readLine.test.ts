bdf511001c94012d1e3117b5eedb125779df9afc
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-02-13 10:35:23 +0900

    [fix] readLine関数のテストを修正

--- __test__/utils/readLine.test.ts
+++ __test__/utils/readLine.test.ts
@@ -4,10 +4,10 @@ describe("readLine関数", () => {
   it("標準入力から入力を受け取り、文字列に変換したあとtrim()メソッドを実行してその値を返す", async () => {
     const stdin = require("mock-stdin").stdin();
     process.nextTick(() => {
-      stdin.send(`test`);
+      stdin.send("input");
     });
 
     const result = await readLine();
-    expect(result).toBe(`test`);
+    expect(result).toBe("input");
   });
 });
