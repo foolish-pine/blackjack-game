e4cd7cca5742d691bffe102c2f9adaed1c041df0
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-02-17 10:43:14 +0900

    [fix] printLine関数のテストでテストごとにモックをリセットするよう修正

--- __test__/utils/printLine.test.ts
+++ __test__/utils/printLine.test.ts
@@ -1,10 +1,15 @@
 import { printLine } from "../../src/utils/printLine";
 
+const mockProcessStdoutWrite = jest.spyOn(process.stdout, "write");
+
+afterEach(() => {
+  mockProcessStdoutWrite.mockReset();
+});
+
 describe("printLine関数", () => {
   it("printLine()メソッドに渡された引数を引数にして、process.stdout.write()メソッドを1度呼び出す", () => {
-    const mockProcessStdoutWrite = jest
-      .spyOn(process.stdout, "write")
-      .mockImplementation();
+    mockProcessStdoutWrite.mockImplementation();
+
     printLine("text");
     expect(mockProcessStdoutWrite).toHaveBeenCalledTimes(1);
     expect(mockProcessStdoutWrite).toHaveBeenCalledWith("text");
